9fd0b8dacdc509bb7bcc335b8d11e89c3a8042b7
ngocaovien <ngocaovien@gmail.com>
2018-06-03 19:58:17 -0700

    second attempt

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // //below are my favorite movies in their individual array.
 // let favMov1 =["Star Wars", "Luke"];
 // let favMov2 =["Hero", "Broken Sword"];
@@ -25,9 +26,35 @@
 // let favMovLength =favoriteMovies.length; //the favorite movie length is now only 3 as opposed to 5 earlier
 // alert(`The length of the favoriteMovies array is ${favMovLength}`); //alerting the length of the favorite movie.
 
+
+//below are my favorite movies in their individual array.
+let favMov1 =["Star Wars", "Luke"];
+let favMov2 =["Hero", "Broken Sword"];
+let favMov3 =["2046", "Wo Man"];
+let favMov4 =["Tampopo", "Gangster"];
+let favMov5 =["Journey to the West", "Monkey"];
+//declaring favoriteMovies to be all of my favorite movies.
+let favoriteMovies = [];
+favoriteMovies.push(favMov1,favMov2, favMov3, favMov4, favMov5);
+console.log(favoriteMovies); //logging my favoriteMovies
+
+
 function ageInDogYears(regularYears) {
   return regularYears * 7;
 }
 
+
 var dogYears = ageInDogYears(2);
 alert dogYears;
+
+//below is the array just for the titles.
+let titles= [];
+titles.push(favoriteMovies[0][0], favoriteMovies [1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0]);
+alert(titles);
+
+//using splice to create an array of just my favorite and least favorite movies.
+let favAndLeast = favoriteMovies.splice(2,2); //using splice to get the favorite and least favorite movies. Storing them in favAndLeast variable.
+console.log(favAndLeast); // console logging my favorite and least favorite film.
+
+let favMovLength =favoriteMovies.length; //the favorite movie length is now only 3 as opposed to 5 earlier
+alert(`The length of the favoriteMovies array is ${favMovLength}`); //alerting the length of the favorite movie.
